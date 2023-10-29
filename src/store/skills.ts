7533592c806180a89d4346fdd9db8862be0c8397
tung le <tung.le0319@gmail.com>
2023-10-29 14:32:18 -0600

    Remodified several components

---
+++ src/store/skills.ts
@@ -0,0 +1,117 @@
+export const skillsIcons = [
+  {
+    title: "HTML5",
+    src: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png",
+  },
+  {
+    title: "CSS3",
+    src: "https://armortechs.com/upload/image/blog/css-variables-css-3-logo.png",
+  },
+  {
+    title: "JavaScript ES6",
+    src: "https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-512.png",
+  },
+  {
+    title: "C-Sharp",
+    src: "https://1.bp.blogspot.com/-U8C6UaajxeA/XV1J6Yo6x2I/AAAAAAAABQo/3v2OEgTGn9ggUqArXtGZpl_6zqbo09nmACLcBGAs/s1600/csharp_logo.png",
+  },
+  {
+    title: "Node Js",
+    src: "https://walde.co/wp-content/uploads/2016/09/nodejs_logo.png",
+  },
+  {
+    title: "Vue.js 3",
+    src: "https://imgs.search.brave.com/Hsy3vsixUOO8EgH22as2LpgNP6zF5hEx6P-pDoc0n0c/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9kMzN3/dWJyZmtpMGw2OC5j/bG91ZGZyb250Lm5l/dC83MThjNmU0ODk1/NjI0OTEyMWNmM2Nj/YTFhMjJhOTlmODM3/MmEzZjBkLzVmNjg5/L19udXh0L2ltZy9j/MmE2MDVmLnBuZw",
+  },
+  {
+    title: "Svelte",
+    src: "https://imgs.search.brave.com/AtFnftxXfySR-pIt0LrD64jshxNlrFjMfr9etQbLNAU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9yYXcu/Z2l0aHVidXNlcmNv/bnRlbnQuY29tL2dp/bGJhcmJhcmEvbG9n/b3MvbWFpbi9sb2dv/cy9zdmVsdGUtaWNv/bi5zdmc.svg",
+  },
+  {
+    title: "React",
+    src: "https://imgs.search.brave.com/qlFAtMUjOJ06EBhykDOXGUrmQ4UM2GdOmNiPAzXjS5w/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvUmVhY3Rf/bG9nb19sb2dvdHlw/ZV9lbWJsZW0tNzAw/eDYyNi5wbmc",
+  },
+  {
+    title: "Angular",
+    src: "https://imgs.search.brave.com/YlS5VuWzZvKwp2RgiANLb12xGWHVnPPV6Id32uidwng/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jZG4u/ZnJlZWJpZXN1cHBs/eS5jb20vbG9nb3Mv/bGFyZ2UvMngvYW5n/dWxhci1pY29uLTEt/bG9nby1wbmctdHJh/bnNwYXJlbnQucG5n",
+  },
+  {
+    title: "Next.js",
+    src: "https://seeklogo.com/images/N/next-js-icon-logo-EE302D5DBD-seeklogo.com.png",
+  },
+  {
+    title: "Nuxt.js",
+    src: "https://imgs.search.brave.com/ac3WGz-DgM03071K34NEHSDn6LTb8cM2YIrFfa7YAcM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9h/L2FlL051eHRfbG9n/by5zdmc.svg",
+  },
+  {
+    title: "DotNet Core",
+    src: "https://wpguru.co.uk/wp-content/uploads/2020/04/dotnet-logo.png",
+  },
+  {
+    title: "BootStrap",
+    src: "https://pluspng.com/img-png/bootstrap-logo-vector-png-bem-with-bootstrap-bootstrap-logo-1024.png",
+  },
+  {
+    title: "TailWind CSS",
+    src: "https://media.zeemly.com/zeemly/product/tailwind-css.png",
+  },
+  {
+    title: "Sass",
+    src: "https://vanseodesign.com/blog/wp-content/uploads/2015/09/sass-logo-2.png",
+  },
+  {
+    title: "MongoDb",
+    src: "https://pluspng.com/img-png/logo-mongodb-png-hundreds-of-popular-mongodb-articles-400.png",
+  },
+
+  {
+    title: "MySQL",
+    src: "https://findicons.com/files/icons/977/rrze/720/database_mysql.png",
+  },
+  {
+    title: "PostgreSQL",
+    src: "https://cdn.freebiesupply.com/logos/thumbs/2x/postgresql-logo.png",
+  },
+  {
+    title: "Prisma.io",
+    src: "https://imgs.search.brave.com/KhQAV89DalGsyHLIk36WzppNhJCg02NbIehcQjuSTeU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pY29u/cy52ZXJ5aWNvbi5j/b20vcG5nL28vYnVz/aW5lc3MvdnNjb2Rl/LXByb2dyYW0taXRl/bS1pY29uL3ByaXNt/YS5wbmc",
+  },
+  {
+    title: "Auth0",
+    src: "https://www.pngrepo.com/png/349297/512/auth0.png",
+  },
+  {
+    title: "Auth.js",
+    src: "https://authjs.dev/img/logo/logo-sm.webp",
+  },
+
+  {
+    title: "Github",
+    src: "https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg",
+  },
+
+  {
+    title: "Git",
+    src: "https://3.bp.blogspot.com/-xhNpNJJyQhk/XIe4GY78RQI/AAAAAAAAItc/ouueFUj2Hqo5dntmnKqEaBJR4KQ4Q2K3ACK4BGAYYCw/s1600/logo%2Bgit%2Bicon.png",
+  },
+  {
+    title: "Postman",
+    src: "https://blog.scottlogic.com/smangan/assets/postman.png",
+  },
+  {
+    title: "Figma",
+    src: "https://cdn.iconscout.com/icon/free/png-512/figma-1693589-1442630.png",
+  },
+  {
+    title: "FireBase",
+    src: "https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png",
+  },
+  {
+    title: "PocketBase",
+    src: "https://seeklogo.com/images/P/pocketbase-logo-CA73994F09-seeklogo.com.png",
+  },
+  {
+    title: "Trello",
+    src: "https://pluspng.com/img-png/trello-png-trello-png-1600.png",
+  },
+];
