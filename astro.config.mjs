d76e9d59b0827c81bea7818aab7b579c7b282862
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2024-07-02 18:05:16 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://v2.daniel.devopportunities.dev/",
+  site: "https://daniel.devopportunities.dev/",
   integrations: [tailwind()],
   vite: {
     ssr: {
